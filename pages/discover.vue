21b5c039cbfd8c9183965798dc38490b72b506ee
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-06 11:29:45 +0200

    added discover page

---
+++ pages/discover.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/topics">{{ $t("topics.title") }}</nuxt-link>
+    <nuxt-link to="/devils_advocates">
+      {{
+      $t("devilsAdvocates.title")
+      }}
+    </nuxt-link>
+    <nuxt-link to="/external_resources">
+      {{
+      $t("externalResources.title")
+      }}
+    </nuxt-link>
+    <div class="underline">{{ this.$store.state.devilsAdvocates }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Tag } from "~/types";
+import tags from "~/static/data/tags.json";
+
+export default Vue.extend({});
+</script>
+
+<style></style>
