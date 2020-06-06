d048e1f4fad834d60833d19bebbdbce39de71362
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-06 16:25:26 +0200

    finished discover page

--- pages/discover.vue
+++ pages/discover.vue
@@ -1,26 +1,52 @@
 <template>
-  <div class="container">
-    <nuxt-link to="/topics">{{ $t("topics.title") }}</nuxt-link>
-    <nuxt-link to="/devils_advocates">
-      {{
-      $t("devilsAdvocates.title")
-      }}
-    </nuxt-link>
-    <nuxt-link to="/external_resources">
-      {{
-      $t("externalResources.title")
-      }}
-    </nuxt-link>
-    <div class="underline">{{ this.$store.state.devilsAdvocates }}</div>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <HomeButton />
+
+    <div>
+      <Link
+        v-for="link in links"
+        :key="link.link"
+        :link="link.link"
+        :label="link.label"
+        class="mb-32 last:mb-0"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Icon from "~/components/Icon.vue";
+import HomeButton from "~/components/HomeButton.vue";
+import Link from "~/components/Link.vue";
 import { Tag } from "~/types";
 import tags from "~/static/data/tags.json";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: {
+    Icon,
+    HomeButton,
+    Link
+  },
+  data() {
+    return {
+      links: [
+        {
+          link: "/topics",
+          label: "topics.title"
+        },
+        {
+          link: "/devils_advocates",
+          label: "devilsAdvocates.title"
+        },
+        {
+          link: "/external_resources",
+          label: "externalResources.title"
+        }
+      ]
+    };
+  }
+});
 </script>
 
 <style></style>
