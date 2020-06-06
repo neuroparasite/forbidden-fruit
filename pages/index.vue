4386eb23ba82ae5a87489c4c34806e6266f066ca
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-06 11:32:48 +0200

    added home page; added styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,43 @@
 <template>
-  <div class="container">
-    <nuxt-link to="/topics">
-      {{ $t("topics.title") }}
-    </nuxt-link>
-    <nuxt-link to="/devils_advocates">{{
-      $t("devilsAdvocates.title")
-    }}</nuxt-link>
-    <nuxt-link to="/external_resources">{{
-      $t("externalResources.title")
-    }}</nuxt-link>
-    <div class="underline">{{ this.$store.state.devilsAdvocates }}</div>
+  <div>
+    <div class="flex flex-col items-center text-center h-screen">
+      <div class="mt-56 mx-64">
+        <div class="text-12 mb-12">{{ $t("home.quote") }}</div>
+        <div class="text-10">{{ $t("home.quoteSource") }}</div>
+      </div>
+
+      <div
+        class="flex items-center justify-center mt-128 text-24 text-secondary font-black tracking-tighter leading-none w-fruit h-fruit rounded-full bg-secondary"
+      >
+        <nuxt-link class="w-fruitText" to="/discover">{{ $t("home.discover") }}</nuxt-link>
+      </div>
+
+      <div class="flex flex-col items-center absolute bottom-20">
+        <div class="text-12 mb-8">{{ $t("scroll.down") }}</div>
+        <Icon class="h-24 w-24" type="arrowDown" />
+      </div>
+    </div>
+
+    <div class="flex flex-col items-center justify-center h-screen">
+      <div
+        class="flex flex-col items-center justify-center h-circleLarge w-circleLarge border-3 border-primary border-solid rounded-full"
+      >
+        <div class="flex flex-col px-48">
+          <div>{{ $t("info.description")}}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import fs from "fs";
-import Logo from "~/components/Logo.vue";
-import { Tag } from "~/types";
-import tags from "~/static/data/tags.json";
+import Icon from "~/components/Icon.vue";
 
 export default Vue.extend({
   components: {
-    Logo,
-  },
+    Icon
+  }
 });
 </script>
 
