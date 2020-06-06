e612409b43f3ba8ee852b1c625eca61ac24fa72a
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-06 17:28:36 +0200

    finished topics page

--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    nonono
-    <nuxt-link v-for="topic in topics" :key="topic.id" :to="`/topics/${topic.name}/`">
-      <div v-on:click="setCurrentTopic(topic.name)">{{ topic.name }}</div>
-    </nuxt-link>
+  <div class="h-screen">
+    <HomeButton>
+      <BackLink class="ml-32" link="/discover" />
+    </HomeButton>
+
+    <FHeader title="topics.title" />
+
+    <div class="flex flex-col items-center justify-center mt-56">
+      <div>
+        <Link
+          v-for="topic in topics"
+          :key="topic.id"
+          :link="topic.name"
+          :label="topic.label"
+          class="mb-32 last:mb-0"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { topics } from "~/types";
+import HomeButton from "~/components/HomeButton.vue";
+import Link from "~/components/Link.vue";
+import BackLink from "~/components/BackLink.vue";
+import FHeader from "~/components/FHeader.vue";
 
 export default Vue.extend({
+  components: {
+    HomeButton,
+    Link,
+    BackLink,
+    FHeader
+  },
   data() {
     return {
       topics: topics
