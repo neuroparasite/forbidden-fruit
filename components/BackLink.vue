2f1316a271f8d92a0d0258af3740c56675efb4ba
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-06 17:17:24 +0200

    added BackLink component

---
+++ components/BackLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="navigateTo()" class="flex items-center">
+    <Icon class="h-24 w-24 mr-12" type="backLink" />
+    <div>{{ $t("ui.back") }}</div>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Icon from "~/components/Icon.vue";
+
+const BackLink = Vue.extend({
+  components: {
+    Icon
+  },
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    navigateTo() {
+      window.location.href = this.link;
+    }
+  }
+});
+
+export default BackLink;
+</script>
+
+<style></style>
