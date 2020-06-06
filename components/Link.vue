71f8aea9d1f1f1289a47940da6ad7990ca893ed2
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-06 16:24:47 +0200

    added Link component

---
+++ components/Link.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center">
+    <Icon class="h-24 w-24 mr-12" type="link" />
+    <nuxt-link :to="link">{{ $t(label) }}</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Icon from "~/components/Icon.vue";
+
+const Link = Vue.extend({
+  components: {
+    Icon
+  },
+  props: {
+    link: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  }
+});
+
+export default Link;
+</script>
+
+<style></style>
