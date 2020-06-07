ea1ce87baedc51dc201a0bb8eaf3a73eae91c31d
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-07 16:07:25 +0200

    extracted GoToTopButton from FHeader component

--- components/FHeader.vue
+++ components/FHeader.vue
@@ -1,25 +1,19 @@
 <template>
-  <div class="mt-12 ml-64 flex">
-    <div class="flex flex-col" :class="{ 'mr-32': goToTop }">
+  <div class="mt-24 ml-48 flex">
+    <div class="flex flex-col">
       <div class="font-black text-32">{{ $t(title) }}</div>
       <div v-if="subtitle" class="flex">
         <div class="text-24 mr-20">{{ $t(subtitle) }}</div>
         <div v-if="subsubtitle" class="text-12">{{ $t(subsubtitle) }}</div>
       </div>
     </div>
-
-    <GoToTopButton v-if="goToTop" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import GoToTopButton from "~/components/GoToTopButton.vue";
 
 const FHeader = Vue.extend({
-  components: {
-    GoToTopButton
-  },
   props: {
     title: {
       type: String,
