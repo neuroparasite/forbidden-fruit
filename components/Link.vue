2cfdd699b0d38306e5ee11c29e9574a98400aeee
fuchsh <fuchsh@Tests-MBP.fritz.box>
2020-06-07 16:11:19 +0200

    moved bottom margin of Link component

--- components/Link.vue
+++ components/Link.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="navigateTo()"
-    class="flex items-center"
+    class="flex items-center mb-32 last:mb-0"
     :class="{ 'text-disabled': disabled, 'pointer-events-none': disabled }"
   >
     <Icon class="h-24 w-24 mr-12" type="link" />
