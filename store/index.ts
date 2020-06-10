9d8b02d73dc360ec27842c0922443e65c983b9b1
fuchsh <fuchsh@Tests-MacBook-Pro.local>
2020-06-10 12:31:25 +0200

    added methods resetState, setCurrentSubtopic + added getters

--- store/index.ts
+++ store/index.ts
@@ -1,28 +1,50 @@
 import devils_advocates from "~/static/data/devils_advocates.json";
 import tags from "~/static/data/tags.json";
 import topics from "~/static/data/topics.json";
-import { State } from "~/types";
+import { Topic, State, Subtopic } from "~/types";
 
 export const state = (): State => ({
   currentTopic: undefined,
-  currentResourceType: "",
+  currentSubtopic: undefined,
 
-  topics: topics,
+  topics,
   devilsAdvocates: devils_advocates,
-  tags: tags,
+  tags,
 });
 
 export const mutations = {
-  setCurrentTopic(state: State, topicName: string) {
-    state.currentTopic = state.topics.find((t) => t.name === topicName);
-  },
-  resetCurrentTopic(state: State) {
+  resetState(state: State) {
     state.currentTopic = undefined;
+    state.currentSubtopic = undefined;
   },
-  setCurrentResourceType(state: State, resourceType: string) {
-    state.currentResourceType = resourceType;
+  setCurrentTopic(state: State, topicName: string) {
+    state.currentTopic = state.topics.find((t: Topic) => t.name === topicName);
   },
-  resetCurrentResourceType(state: State) {
-    state.currentResourceType = "";
+  setCurrentSubtopic(state: State, subtopicName: string) {
+    state.currentSubtopic = state.currentTopic?.subtopics.find(
+      (s: Subtopic) => s.name === subtopicName
+    );
+  },
+};
+
+export const getters = {
+  currentTopic: (state: State) => {
+    return state.currentTopic;
+  },
+  currentSubtopic: (state: State) => {
+    return state.currentSubtopic;
+  },
+  topics: (state: State) => {
+    return state.topics;
+  },
+  subtopics: (_: State, getters: any) => (topicName: string) => {
+    const topic = getters["topics"].find((t: Topic) => t.name === topicName);
+    return topic?.subtopics;
+  },
+  devilsAdvocates: (state: State) => {
+    return state.devilsAdvocates;
+  },
+  tags: (state: State) => {
+    return state.tags;
   },
 };
