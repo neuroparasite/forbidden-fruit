9822b87d1bf81f818f43beaf07995818b877f05e
fuchsh <fuchsh@Tests-MacBook-Pro.local>
2020-06-11 16:57:25 +0200

    fixed GoToTopButton goToTop method

--- components/GoToTopButton.vue
+++ components/GoToTopButton.vue
@@ -31,7 +31,11 @@ export default class GoToTopButton extends Vue {
   }
 
   goToTop() {
-    document.body.scrollTop = 0;
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   }
 
   created() {
