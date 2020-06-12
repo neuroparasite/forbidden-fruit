320af22479d3bdd1f9fc18fd93bb1c1b9476121b
fuchsh <fuchsh@Tests-MacBook-Pro.local>
2020-06-12 18:42:42 +0200

    added FWikipediaLink component

---
+++ components/FWikipediaLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <a
+    :href="$t(i18nKey)"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="py-4 px-4 border-primary border-1 border-solid"
+  >
+    <Icon class="h-32 w-32" type="wikipedia" />
+  </a>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+import Icon from "~/components/Icon.vue";
+
+@Component({
+  components: {
+    Icon,
+  },
+})
+export default class FWikipediaLink extends Vue {
+  @Prop({ required: true })
+  i18nKey!: string;
+}
+</script>
+
+<style></style>
