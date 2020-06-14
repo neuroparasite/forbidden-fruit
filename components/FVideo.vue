68d41ae2bfd3bfe7d2c36101e045dc0b24bedde5
fuchsh <fuchsh@Tests-MacBook-Pro.local>
2020-06-14 10:50:52 +0200

    added FVideo component

---
+++ components/FVideo.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="flex flex-col p-16 bg-primary-light" :id="video.i18nKey">
+    <div class="flex justify-between text-18 text-accent mb-8">
+      <div>{{ video.year }}</div>
+      <div v-if="devilsAdvocatesByKeys" class="flex flex-col">
+        <div
+          v-for="devilsAdvocate in devilsAdvocatesByKeys"
+          :key="devilsAdvocate.i18nKey"
+          class="mb-4 last:mb-0"
+        >
+          <div>
+            {{ devilsAdvocate.firstname + " " + devilsAdvocate.lastname }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="font-medium text-18 mb-16">{{ video.title }}</div>
+
+    <iframe
+      width="100%"
+      height="100%"
+      :src="video.embedUrl"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="mb-16"
+    ></iframe>
+
+    <div class="grid grid-cols-3 gap-16 mb-16">
+      <FTag v-for="tag in tagsByKeys" :key="tag.i18nKey" :tag="tag" />
+    </div>
+
+    <div class="flex justify-start text-accent">
+      <button class="flex items-center p-4" @click="copyLink()">
+        <Icon class="h-16 w-16 mr-8" type="copy" />
+        <div>{{ $t("ui.copyLink") }}</div>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { mapGetters } from "vuex";
+
+import Icon from "~/components/Icon.vue";
+import FTag from "~/components/FTag.vue";
+import { Tag, Video } from "~/types";
+
+@Component({
+  components: {
+    Icon,
+    FTag,
+  },
+})
+export default class FVideo extends Vue {
+  @Prop({ required: true })
+  public video!: Video;
+
+  get videoURL(): string {
+    return `https://forbidden-fruit.now.sh${this.$route.path}#${this.video.i18nKey}`;
+  }
+
+  get tagsByKeys(): Tag {
+    return this.$store.getters["tagsByKeys"](this.video.tagKeys);
+  }
+
+  get devilsAdvocatesByKeys(): Tag {
+    return this.$store.getters["devilsAdvocatesByKeys"](
+      this.video.devilsAdvocateKeys
+    );
+  }
+
+  copyLink() {
+    navigator.clipboard.writeText(this.videoURL).then(
+      () => {
+        /* clipboard successfully set */
+      },
+      () => {
+        /* clipboard write failed */
+      }
+    );
+  }
+}
+</script>
+
+<style></style>
