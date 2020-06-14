d1f3422e6d06d15cca0523731c14a4ab645c0ef0
fuchsh <fuchsh@Tests-MacBook-Pro.local>
2020-06-14 11:47:09 +0200

    added FExternalLink component

---
+++ components/FExternalLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <a
+    :href="link"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="flex items-center p-4 mb-24"
+  >
+    <Icon class="h-20 w-20 mr-8" type="linkExternal" />
+    <div>{{ $t(label) }}</div>
+  </a>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { mapGetters } from "vuex";
+
+import Icon from "~/components/Icon.vue";
+
+@Component({
+  components: {
+    Icon,
+  },
+})
+export default class FExternalLink extends Vue {
+  @Prop({ required: true })
+  link!: string;
+
+  @Prop({ required: true })
+  label!: string;
+}
+</script>
