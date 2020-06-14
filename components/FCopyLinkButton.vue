4397c7d813ead9edbcbf5f5ccc48b219b45bce91
fuchsh <fuchsh@Tests-MacBook-Pro.local>
2020-06-14 12:06:32 +0200

    added FCopyLinkButton component

---
+++ components/FCopyLinkButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="flex items-center p-4" @click="copyLink()">
+    <Icon class="h-16 w-16 mr-8" type="copy" />
+    <div>{{ $t("ui.copyLink") }}</div>
+  </button>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+import Icon from "~/components/Icon.vue";
+
+@Component({
+  components: {
+    Icon,
+  },
+})
+export default class FCopyLinkButton extends Vue {
+  @Prop({ required: true })
+  link!: string;
+
+  copyLink() {
+    navigator.clipboard.writeText(this.link).then(
+      () => {
+        /* clipboard successfully set */
+      },
+      () => {
+        /* clipboard write failed */
+      }
+    );
+  }
+}
+</script>
